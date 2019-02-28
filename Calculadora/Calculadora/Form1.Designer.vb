cdfdb0e65a50de59641c8fd45d27f35a6e511380
Gerson Chin <47795751+G7irca@users.noreply.github.com>
2019-02-28 13:04:27 +0200

    Calculadora
    Gerson chin

---
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.num7 = New System.Windows.Forms.Button()
+        Me.equal = New System.Windows.Forms.Button()
+        Me.num3 = New System.Windows.Forms.Button()
+        Me.num6 = New System.Windows.Forms.Button()
+        Me.num9 = New System.Windows.Forms.Button()
+        Me.num5 = New System.Windows.Forms.Button()
+        Me.dot = New System.Windows.Forms.Button()
+        Me.num2 = New System.Windows.Forms.Button()
+        Me.num8 = New System.Windows.Forms.Button()
+        Me.num0 = New System.Windows.Forms.Button()
+        Me.num1 = New System.Windows.Forms.Button()
+        Me.num4 = New System.Windows.Forms.Button()
+        Me.add = New System.Windows.Forms.Button()
+        Me.subtract = New System.Windows.Forms.Button()
+        Me.multi = New System.Windows.Forms.Button()
+        Me.divide = New System.Windows.Forms.Button()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.Nclear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'num7
+        '
+        Me.num7.Location = New System.Drawing.Point(7, 108)
+        Me.num7.Name = "num7"
+        Me.num7.Size = New System.Drawing.Size(90, 43)
+        Me.num7.TabIndex = 0
+        Me.num7.Text = "7"
+        Me.num7.UseVisualStyleBackColor = True
+        '
+        'equal
+        '
+        Me.equal.Location = New System.Drawing.Point(304, 206)
+        Me.equal.Name = "equal"
+        Me.equal.Size = New System.Drawing.Size(90, 92)
+        Me.equal.TabIndex = 2
+        Me.equal.Text = "="
+        Me.equal.UseVisualStyleBackColor = True
+        '
+        'num3
+        '
+        Me.num3.Location = New System.Drawing.Point(200, 206)
+        Me.num3.Name = "num3"
+        Me.num3.Size = New System.Drawing.Size(90, 43)
+        Me.num3.TabIndex = 3
+        Me.num3.Text = "3"
+        Me.num3.UseVisualStyleBackColor = True
+        '
+        'num6
+        '
+        Me.num6.Location = New System.Drawing.Point(200, 157)
+        Me.num6.Name = "num6"
+        Me.num6.Size = New System.Drawing.Size(90, 43)
+        Me.num6.TabIndex = 4
+        Me.num6.Text = "6"
+        Me.num6.UseVisualStyleBackColor = True
+        '
+        'num9
+        '
+        Me.num9.Location = New System.Drawing.Point(200, 108)
+        Me.num9.Name = "num9"
+        Me.num9.Size = New System.Drawing.Size(90, 43)
+        Me.num9.TabIndex = 5
+        Me.num9.Text = "9"
+        Me.num9.UseVisualStyleBackColor = True
+        '
+        'num5
+        '
+        Me.num5.Location = New System.Drawing.Point(103, 157)
+        Me.num5.Name = "num5"
+        Me.num5.Size = New System.Drawing.Size(90, 43)
+        Me.num5.TabIndex = 6
+        Me.num5.Text = "5"
+        Me.num5.UseVisualStyleBackColor = True
+        '
+        'dot
+        '
+        Me.dot.Location = New System.Drawing.Point(199, 255)
+        Me.dot.Name = "dot"
+        Me.dot.Size = New System.Drawing.Size(90, 43)
+        Me.dot.TabIndex = 7
+        Me.dot.Text = "."
+        Me.dot.UseVisualStyleBackColor = True
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(103, 206)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(90, 43)
+        Me.num2.TabIndex = 8
+        Me.num2.Text = "2"
+        Me.num2.UseVisualStyleBackColor = True
+        '
+        'num8
+        '
+        Me.num8.Location = New System.Drawing.Point(103, 108)
+        Me.num8.Name = "num8"
+        Me.num8.Size = New System.Drawing.Size(90, 43)
+        Me.num8.TabIndex = 9
+        Me.num8.Text = "8"
+        Me.num8.UseVisualStyleBackColor = True
+        '
+        'num0
+        '
+        Me.num0.Location = New System.Drawing.Point(103, 255)
+        Me.num0.Name = "num0"
+        Me.num0.Size = New System.Drawing.Size(90, 43)
+        Me.num0.TabIndex = 10
+        Me.num0.Text = "0"
+        Me.num0.UseVisualStyleBackColor = True
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(7, 206)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(90, 43)
+        Me.num1.TabIndex = 11
+        Me.num1.Text = "1"
+        Me.num1.UseVisualStyleBackColor = True
+        '
+        'num4
+        '
+        Me.num4.Location = New System.Drawing.Point(7, 157)
+        Me.num4.Name = "num4"
+        Me.num4.Size = New System.Drawing.Size(90, 43)
+        Me.num4.TabIndex = 12
+        Me.num4.Text = "4"
+        Me.num4.UseVisualStyleBackColor = True
+        '
+        'add
+        '
+        Me.add.Location = New System.Drawing.Point(304, 157)
+        Me.add.Name = "add"
+        Me.add.Size = New System.Drawing.Size(90, 43)
+        Me.add.TabIndex = 13
+        Me.add.Text = "+"
+        Me.add.UseVisualStyleBackColor = True
+        '
+        'subtract
+        '
+        Me.subtract.Location = New System.Drawing.Point(304, 108)
+        Me.subtract.Name = "subtract"
+        Me.subtract.Size = New System.Drawing.Size(90, 43)
+        Me.subtract.TabIndex = 14
+        Me.subtract.Text = "-"
+        Me.subtract.UseVisualStyleBackColor = True
+        '
+        'multi
+        '
+        Me.multi.Location = New System.Drawing.Point(304, 59)
+        Me.multi.Name = "multi"
+        Me.multi.Size = New System.Drawing.Size(90, 43)
+        Me.multi.TabIndex = 15
+        Me.multi.Text = "*"
+        Me.multi.UseVisualStyleBackColor = True
+        '
+        'divide
+        '
+        Me.divide.Location = New System.Drawing.Point(304, 10)
+        Me.divide.Name = "divide"
+        Me.divide.Size = New System.Drawing.Size(90, 43)
+        Me.divide.TabIndex = 16
+        Me.divide.Text = "/"
+        Me.divide.UseVisualStyleBackColor = True
+        '
+        'TextBox
+        '
+        Me.TextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox.Location = New System.Drawing.Point(8, 10)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(282, 92)
+        Me.TextBox.TabIndex = 17
+        Me.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'clear
+        '
+        Me.clear.Location = New System.Drawing.Point(1562, 640)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(90, 43)
+        Me.clear.TabIndex = 18
+        Me.clear.Text = "c"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'Nclear
+        '
+        Me.Nclear.Location = New System.Drawing.Point(8, 253)
+        Me.Nclear.Name = "Nclear"
+        Me.Nclear.Size = New System.Drawing.Size(90, 43)
+        Me.Nclear.TabIndex = 19
+        Me.Nclear.Text = "C"
+        Me.Nclear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 308)
+        Me.Controls.Add(Me.Nclear)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.divide)
+        Me.Controls.Add(Me.multi)
+        Me.Controls.Add(Me.subtract)
+        Me.Controls.Add(Me.add)
+        Me.Controls.Add(Me.num4)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.num0)
+        Me.Controls.Add(Me.num8)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.dot)
+        Me.Controls.Add(Me.num5)
+        Me.Controls.Add(Me.num9)
+        Me.Controls.Add(Me.num6)
+        Me.Controls.Add(Me.num3)
+        Me.Controls.Add(Me.equal)
+        Me.Controls.Add(Me.num7)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents num7 As Button
+    Friend WithEvents equal As Button
+    Friend WithEvents num3 As Button
+    Friend WithEvents num6 As Button
+    Friend WithEvents num9 As Button
+    Friend WithEvents num5 As Button
+    Friend WithEvents dot As Button
+    Friend WithEvents num2 As Button
+    Friend WithEvents num8 As Button
+    Friend WithEvents num0 As Button
+    Friend WithEvents num1 As Button
+    Friend WithEvents num4 As Button
+    Friend WithEvents add As Button
+    Friend WithEvents subtract As Button
+    Friend WithEvents multi As Button
+    Friend WithEvents divide As Button
+    Friend WithEvents TextBox As TextBox
+    Friend WithEvents clear As Button
+    Friend WithEvents Nclear As Button
+End Class
---
+++ Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub ButtonClickMethod(sender As Object, e As EventArgs) Handles num0.Click, num1.Click, num2.Click, num3.Click, num4.Click, num5.Click, num6.Click, num7.Click, num8.Click, num9.Click, Nclear.Click, add.Click, multi.Click, subtract.Click, divide.Click, dot.Click, equal.Click
+
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "num1" Then
+            TextBox.Text = TextBox.Text + "1"
+        End If
+        If button.Name = "num2" Then
+            TextBox.Text = TextBox.Text + "2"
+        End If
+        If button.Name = "num3" Then
+            TextBox.Text = TextBox.Text + "3"
+        End If
+        If button.Name = "num4" Then
+            TextBox.Text = TextBox.Text + "5"
+        End If
+        If button.Name = "num5" Then
+            TextBox.Text = TextBox.Text + "4"
+        End If
+        If button.Name = "num6" Then
+            TextBox.Text = TextBox.Text + "6"
+        End If
+        If button.Name = "num7" Then
+            TextBox.Text = TextBox.Text + "7"
+        End If
+        If button.Name = "num8" Then
+            TextBox.Text = TextBox.Text + "8"
+        End If
+        If button.Name = "num9" Then
+            TextBox.Text = TextBox.Text + "9"
+        End If
+        If button.Name = "num0" Then
+            TextBox.Text = TextBox.Text + "0"
+        End If
+
+        If button.Name = "dot" Then
+            TextBox.Text = TextBox.Text + "."
+
+        End If
+        If button.Name = "add" Then
+            TextBox.Text = TextBox.Text + "+"
+        End If
+        If button.Name = "subtract" Then
+            TextBox.Text = TextBox.Text + "-"
+        End If
+        If button.Name = "divide" Then
+            TextBox.Text = TextBox.Text + "/"
+        End If
+        If button.Name = "multi" Then
+            TextBox.Text = TextBox.Text + "*"
+        End If
+        If button.Name = "Nclear" Then
+            TextBox.Text = TextBox.Text + ""
+        End If
+        If button.Name = "equal" Then
+            Dim equation As String = TextBox.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox.Text = result
+        End If
+
+    End Sub
+End Class
